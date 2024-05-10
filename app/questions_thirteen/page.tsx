40e1fe276ec766dc39c36db92a8d73dea64fcab8
suad <suad.demiri@it.htlhl.at>
2024-05-10 17:24:13 +0200

    letze aenderungen + primeicons ud primereact added

--- app/questions_thirteen/page.tsx
+++ app/questions_thirteen/page.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+"use client";
 
+import React, { useState } from "react";
 function page() {
-  return (
-    <div>page</div>
-  )
+  const [selectedDistrict, setSelectedDistrict] = useState<any>(null);
+
+  const districts = [
+    { name: "Innere Stadt (Inner City)", code: 1 },
+    { name: "Leopoldtstadt", code: 2 },
+    { name: "Landstraße", code: 3 },
+  ];
+
+  return <div>page</div>;
 }
 
-export default page
+export default page;
