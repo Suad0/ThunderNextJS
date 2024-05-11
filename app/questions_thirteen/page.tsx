e82dcb2ce605547b61c6751b544a7d157f5ad0dc
suad <suad.demiri@it.htlhl.at>
2024-05-11 13:03:09 +0200

    card 3d effect

--- app/questions_thirteen/page.tsx
+++ app/questions_thirteen/page.tsx
@@ -5,6 +5,7 @@ import { Dropdown } from "primereact/dropdown";
 
 function Page() {
   const [selectedDistrict, setSelectedDistrict] = useState<any>(null);
+  const [selectedBudget, setSelectedBudget] = useState<any>(null);
 
   const districts = [
     { name: "Innere Stadt (Inner City)", code: 1 },
@@ -16,18 +17,45 @@ function Page() {
     setSelectedDistrict(e.value);
   };
 
+  const budgetForRoom = [
+    { name: "€ 400 - 480", code: 1 },
+    { name: "€ 480 - 540", code: 2 },
+    { name: "€ 540 - 660", code: 3 },
+  ];
+  const onBudgetChange = (e: { value: any }) => {
+    setSelectedBudget(e.value);
+  };
+
   return (
-    <div className="card">
-      <div className="flex justify-center items-center h-screen">
-        <div className="w-64">
-          <Dropdown
-            value={selectedDistrict}
-            options={districts}
-            onChange={onCityChange}
-            optionLabel="name"
-            placeholder="Select a District"
-            className="w-full"
-          />
+    <div className="flex justify-center items-center h-screen">
+      <div className="card-container">
+        <div className="card">
+          <div className="mb-4">
+            <div>
+              <h2>What is your Budget for a room</h2>
+              <Dropdown
+                value={selectedBudget}
+                options={budgetForRoom}
+                onChange={onBudgetChange}
+                optionLabel="name"
+                placeholder="Select your Budget"
+                className="w-full"
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <h2>Choose your favorite district </h2>
+              <Dropdown
+                value={selectedDistrict}
+                options={districts}
+                onChange={onCityChange}
+                optionLabel="name"
+                placeholder="Select a District"
+                className="w-full"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
